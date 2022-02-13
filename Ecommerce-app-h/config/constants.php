f150829cbe2cea017496585364eed65cf4043ffc
ndzirr <93895557+ndzirr@users.noreply.github.com>
2022-02-13 12:59:18 +0800

    Add files via upload

---
+++ Ecommerce-app-h/config/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'project1_nadzir1');
+define('PASSWORD', 'UdU}O+t!F&r=');
+define('DATABASE_NAME', 'project1_ecommerceapp');
+
+define('CURRENCY', 'MYR');
+
+
+
+?>
